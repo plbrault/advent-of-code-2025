c696c62245176d13df39b662364075e79daa331a
Pier-Luc Brault <pier-luc@brault.me>
2025-12-02 20:26:43 -0500

    Day 2 (#2)
    * Parse file
    
    * Remove \n
    
    * Generate individual IDs
    
    * is_invalid detects any number of repetitions
    
    * Part 1 solved
    
    * Try to adapt for part 2, part 1 still working
    
    * Fixed
    
    * Calculate completion time

---
+++ 02.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import textwrap
+
+def parse_file(filename):
+    with open(filename, 'r') as input_file:
+        ranges = [r.replace('\n','').split('-') for r in input_file.readline().split(',')]
+        return ranges
+
+def get_individual_ids(ranges):
+    ids = []
+    for r in ranges:
+        for i in range(int(r[0]), int(r[1]) + 1):
+            ids.append(str(i))
+    return ids
+
+def is_invalid(id, max_repeats=float('inf')):
+    num_digits = len(id)
+    for divisor in range(2, num_digits + 1):
+        quotient = num_digits / divisor
+        if quotient.is_integer():
+            parts = textwrap.wrap(id, int(num_digits / divisor))
+            if (len(set(parts)) == 1 and len(parts) <= max_repeats):
+                return True
+    return False
+
+ranges = parse_file('input.txt')
+ids = get_individual_ids(ranges)
+
+start_time = datetime.now()
+
+part1_result = sum([int(id) for id in ids if is_invalid(id, 2)])
+print('Result for part 1:', part1_result)
+
+part2_result = sum([int(id) for id in ids if is_invalid(id)])
+print('Result for part 2:', part2_result)
+
+end_time = datetime.now()
+print('Completed in', (end_time - start_time).total_seconds(), 'seconds.')
