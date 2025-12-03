e882f435ccdad6f2448d106959fe41b149f2a484
Pier-Luc Brault <pier-luc@brault.me>
2025-12-02 20:04:27 -0500

    is_invalid detects any number of repetitions

--- 02.py
+++ 02.py
@@ -1,3 +1,5 @@
+import textwrap
+
 def parse_file(filename):
     with open(filename, 'r') as input_file:
         ranges = [r.replace('\n','').split('-') for r in input_file.readline().split(',')]
@@ -10,7 +12,17 @@ def get_individual_ids(ranges):
             ids.append(str(i))
     return ids
 
+def is_invalid(id):
+    num_digits = len(id)
+    for divisor in range(2, num_digits + 1):
+        quotient = num_digits / divisor
+        if (num_digits / divisor).is_integer():
+            parts = textwrap.wrap(id, int(num_digits / divisor))
+            if (len(set(parts)) == 1):
+                print(parts)
+
 ranges = parse_file('input.txt')
 ids = get_individual_ids(ranges)
 
-print(ids)
+for id in ids:
+    is_invalid(id)
