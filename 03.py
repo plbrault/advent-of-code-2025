d830b9da3d8b7969afd14600fa7e3e80a454504b
Pier-Luc Brault <pier-luc@brault.me>
2025-12-03 18:49:26 -0500

    Create solve_part1 function

--- 03.py
+++ 03.py
@@ -9,20 +9,23 @@ def parse_file(filename):
             banks.append(bank)
     return banks
 
-def find_first_battery(bank):
-    return max(bank[:-1])
-
-def find_second_battery(bank, first_battery_id):
-    return max(bank[(first_battery_id + 1):])
-
 banks = parse_file('input.txt')
 
-total_joltage = 0
-for bank in banks:
-    first_battery = find_first_battery(bank)
-    first_battery_id = bank.index(first_battery)
-    second_battery = find_second_battery(bank, first_battery_id)
-    joltage = first_battery * 10 + second_battery
-    total_joltage += joltage
+def solve_part1():
+    def find_first_battery(bank):
+        return max(bank[:-1])
 
-print('Result:', total_joltage)
+    def find_second_battery(bank, first_battery_id):
+        return max(bank[(first_battery_id + 1):])
+
+    total_joltage = 0
+    for bank in banks:
+        first_battery = find_first_battery(bank)
+        first_battery_id = bank.index(first_battery)
+        second_battery = find_second_battery(bank, first_battery_id)
+        joltage = first_battery * 10 + second_battery
+        total_joltage += joltage
+
+    print('Result (part 1):', total_joltage)
+
+solve_part1()
