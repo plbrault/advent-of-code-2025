c89b488e231a53d1a13d442bb05a0a47b8337528
Pier-Luc Brault <pier-luc@brault.me>
2025-12-02 19:53:28 -0500

    Generate individual IDs

--- 02.py
+++ 02.py
@@ -1,6 +1,16 @@
 def parse_file(filename):
     with open(filename, 'r') as input_file:
-        ranges = [range.replace('\n','').split('-') for range in input_file.readline().split(',')]
+        ranges = [r.replace('\n','').split('-') for r in input_file.readline().split(',')]
         return ranges
 
-print(parse_file('input.txt'))
+def get_individual_ids(ranges):
+    ids = []
+    for r in ranges:
+        for i in range(int(r[0]), int(r[1]) + 1):
+            ids.append(str(i))
+    return ids
+
+ranges = parse_file('input.txt')
+ids = get_individual_ids(ranges)
+
+print(ids)
