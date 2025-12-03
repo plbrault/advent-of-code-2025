558f9f0948df96662000f95e271d7a41bce2c8fb
Pier-Luc Brault <pier-luc@brault.me>
2025-12-02 19:40:27 -0500

    Parse file

---
+++ 02.py
@@ -0,0 +1,6 @@
+def parse_file(filename):
+    with open(filename, 'r') as input_file:
+        ranges = [range.split('-') for range in input_file.readline().split(',')]
+        return ranges
+
+print(parse_file('input.txt'))
