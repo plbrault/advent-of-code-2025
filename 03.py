bd41e2c31244d17f6d0d30b20c0cf69bf6b19f66
Pier-Luc Brault <pier-luc@brault.me>
2025-12-03 18:44:30 -0500

    Solve part 1

--- 03.py
+++ 03.py
@@ -12,7 +12,17 @@ def parse_file(filename):
 def find_first_battery(bank):
     return max(bank[:-1])
 
+def find_second_battery(bank, first_battery_id):
+    return max(bank[(first_battery_id + 1):])
+
 banks = parse_file('input.txt')
 
+total_joltage = 0
 for bank in banks:
-    print(find_first_battery(bank))
+    first_battery = find_first_battery(bank)
+    first_battery_id = bank.index(first_battery)
+    second_battery = find_second_battery(bank, first_battery_id)
+    joltage = first_battery * 10 + second_battery
+    total_joltage += joltage
+
+print('Result:', total_joltage)
