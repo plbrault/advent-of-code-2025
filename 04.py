d596cc982fac54aa490985a4b797a46303fa3eba
Pier-Luc Brault <pier-luc@brault.me>
2025-12-04 20:30:09 -0500

    Part 1 solved

--- 04.py
+++ 04.py
@@ -5,4 +5,42 @@ def parse_file(filename):
             arr.append([character for character in line if character != '\n'])
     return arr
 
-print(parse_file('input.txt'))
+arr = parse_file('input.txt')
+
+def count_accessible_rolls():
+    ROLL = '@'
+
+    deltas = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1)
+    ]
+
+    accessible_rolls = 0
+
+    for y, row in enumerate(arr):
+        for x, value in enumerate(row):
+            if value == ROLL:
+                num_adjacent_rolls = 0
+                for (dy, dx) in deltas:
+                    if (
+                        y + dy >= 0
+                        and y + dy < len(arr)
+                        and x + dx >= 0
+                        and x + dx < len(row)
+                        and arr[y + dy][x + dx] == ROLL
+                    ):
+                        num_adjacent_rolls += 1
+                        if num_adjacent_rolls == 4:
+                            break
+                if num_adjacent_rolls < 4:
+                    accessible_rolls += 1
+
+    return accessible_rolls
+
+print('Result (part 1):', count_accessible_rolls())
