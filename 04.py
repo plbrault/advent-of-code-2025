2b09924a3055a5bb491619f00fb9f5e0a4e281db
Pier-Luc Brault <pier-luc@brault.me>
2025-12-04 20:39:35 -0500

    Refactor to add function can_access_roll

--- 04.py
+++ 04.py
@@ -7,9 +7,10 @@ def parse_file(filename):
 
 arr = parse_file('input.txt')
 
-def count_accessible_rolls():
-    ROLL = '@'
+ROLL = '@'
+NO_ROLL = '.'
 
+def can_access_roll(y, x):
     deltas = [
         (-1, -1),
         (-1, 0),
@@ -20,27 +21,26 @@ def count_accessible_rolls():
         (1, 0),
         (1, 1)
     ]
+    num_adjacent_rolls = 0
+    for (dy, dx) in deltas:
+        if (
+            y + dy >= 0
+            and y + dy < len(arr)
+            and x + dx >= 0
+            and x + dx < len(arr[0])
+            and arr[y + dy][x + dx] == ROLL
+        ):
+            num_adjacent_rolls += 1
+            if num_adjacent_rolls == 4:
+                break
+    return num_adjacent_rolls < 4
 
+def count_accessible_rolls():
     accessible_rolls = 0
-
     for y, row in enumerate(arr):
         for x, value in enumerate(row):
-            if value == ROLL:
-                num_adjacent_rolls = 0
-                for (dy, dx) in deltas:
-                    if (
-                        y + dy >= 0
-                        and y + dy < len(arr)
-                        and x + dx >= 0
-                        and x + dx < len(row)
-                        and arr[y + dy][x + dx] == ROLL
-                    ):
-                        num_adjacent_rolls += 1
-                        if num_adjacent_rolls == 4:
-                            break
-                if num_adjacent_rolls < 4:
-                    accessible_rolls += 1
-
+            if value == ROLL and can_access_roll(y, x):
+                accessible_rolls += 1
     return accessible_rolls
 
 print('Result (part 1):', count_accessible_rolls())
