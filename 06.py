a03f8cf7b08f5d911831a91481ab2b78a619bfab
Pier-Luc Brault <pier-luc@brault.me>
2025-12-06 11:31:50 -0500

    Read operation for each column

--- 06.py
+++ 06.py
@@ -7,4 +7,11 @@ def parse_file(filename):
         for line in open(filename, 'r').readlines()
     ]
 
-print(parse_file('input.txt'))
+def solve_worksheet(worksheet):
+    for col_id in range(len(worksheet[0])):
+        operation = worksheet[len(worksheet) - 1][col_id]
+        print(operation)
+
+data = parse_file('input.txt')
+solve_worksheet(data)
+#print(parse_file('input.txt'))
