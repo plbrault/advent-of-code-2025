bcb8633e5b640a6a7c383beb03a3175fb465b371
Pier-Luc Brault <pier-luc@brault.me>
2025-12-05 19:27:59 -0500

    Parse all file

--- 05.py
+++ 05.py
@@ -1,11 +1,14 @@
 def parse_file(filename):
     fresh_ranges = []
+    available_ids = []
     with open(filename, 'r') as file:
         line = file.readline().replace('\n', '')
         while line != '':
             start, end = [int(value) for value in line.split('-')]
             fresh_ranges.append((start, end))
             line = file.readline().replace('\n', '')
-    return fresh_ranges
+        for line in file:
+            available_ids.append(int(line))
+    return fresh_ranges, available_ids
 
 print(parse_file('input.txt'))
