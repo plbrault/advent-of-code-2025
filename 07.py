80fca61ca58f0e76720d1a37a874ddb7e1c49d88
Pier-Luc Brault <pier-luc@brault.me>
2025-12-07 16:13:03 -0500

    Revert to part 1

--- 07.py
+++ 07.py
@@ -1,94 +1,57 @@
+matrix = [list(line.replace('\n','')) for line in open('input.txt').readlines()]
+
 START = 'S'
 EMPTY_SPACE = '.'
 SPLITTER = '^'
 BEAM = '|'
 
-def get_navigation_functions(matrix):
-    def west(pos):
-        row, col = pos
-        return (row, col - 1) if col > 0 else None
+start_pos = (0, matrix[0].index(START))
 
-    def south(pos):
-        row, col = pos
-        return (row + 1, col) if row < len(matrix) - 1 else None
+def south(pos):
+    row, col = pos
+    return (row + 1, col) if row < len(matrix) - 1 else None
 
-    def east(pos):
-        row, col = pos
-        return (row, col + 1) if col < len(matrix[0]) - 1 else None
+def west(pos):
+    row, col = pos
+    return (row, col - 1) if col > 0 else None
 
-    return (west, south, east)
+def east(pos):
+    row, col = pos
+    return (row, col + 1) if col < len(matrix[0]) - 1 else None
 
-def get_start_pos(matrix):
-    return (0, matrix[0].index(START))
-
-def value(matrix, pos):
+def value(pos):
     if not pos:
         return None
     row, col = pos
     return matrix[row][col]
 
-def update(matrix, pos, value):
+def update(pos, value):
     row, col = pos
     matrix[row][col] = value
 
-def copy(matrix):
-    return [row[:] for row in matrix]
+def print_matrix():
+    print('\n'.join([''.join(row) for row in matrix]))
 
-def matrix_to_str(matrix):
-    return '\n'.join([''.join(row) for row in matrix])
-
-def print_matrix(matrix):
-    print(matrix_to_str(matrix))
-
-def solve_part1(matrix):
-    start_pos = get_start_pos(matrix)
-    west, south, east = get_navigation_functions(matrix)
-
-    def move_beam(beam):
-        if value(matrix, south(beam)) == EMPTY_SPACE:
-            update(matrix, south(beam), BEAM)
-            return move_beam(south(beam))
-        elif value(matrix, south(beam)) == SPLITTER:
-            return split(south(beam))
-        return 0
-
-    def split(splitter):
-        new_beams = []
-        if value(matrix, west(splitter)) == EMPTY_SPACE:
-            update(matrix, west(splitter), BEAM)
-            new_beams.append(west(splitter))
-        if value(matrix, east(splitter)) == EMPTY_SPACE:
-            update(matrix, east(splitter), BEAM)
-            new_beams.append(east(splitter))
-        splits = 0 if len(new_beams) == 0 else 1
-        for beam in new_beams:
-            splits += move_beam(beam)
-        return splits
-
-    print('Result (part 1):', move_beam(start_pos))
-
-def solve_part2(matrix):
-    start_pos = get_start_pos(matrix)
-    west, south, east = get_navigation_functions(matrix)
-
-    def move_beam(beam):
-        if south(beam) is None:
-            return 1
-        if value(matrix, south(beam)) == SPLITTER:
-            return split(south(beam))
+def move_beam(beam=start_pos):
+    if value(south(beam)) == EMPTY_SPACE:
+        update(south(beam), BEAM)
         return move_beam(south(beam))
+    elif value(south(beam)) == SPLITTER:
+        return split(south(beam))
+    return 0
 
-    def split(splitter):
-        timelines = 0
-        if value(matrix, west(splitter)) == EMPTY_SPACE:
-            timelines += move_beam(west(splitter))
-        if value(matrix, east(splitter)) == EMPTY_SPACE:
-            timelines += move_beam(east(splitter))
-        return timelines
+def split(splitter):
+    new_beams = []
+    if value(west(splitter)) == EMPTY_SPACE:
+        update(west(splitter), BEAM)
+        new_beams.append(west(splitter))
+    if value(east(splitter)) == EMPTY_SPACE:
+        update(east(splitter), BEAM)
+        new_beams.append(east(splitter))
+    
+    splits = 0 if len(new_beams) == 0 else 1
+    for beam in new_beams:
+        splits += move_beam(beam)
+    return splits
 
-    print('Result (part 2):', move_beam(start_pos))
-
-matrix_1 = [list(line.replace('\n','')) for line in open('input.txt').readlines()]
-matrix_2 = copy(matrix_1)
-solve_part1(matrix_1)
-solve_part2(matrix_2)
+print('Result (part 1):', move_beam())
