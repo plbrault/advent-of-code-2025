5688ad3420d29d0b0be70114dba24fabb1c0129f
Pier-Luc Brault <pier-luc@brault.me>
2025-12-07 12:46:12 -0500

    Add navigation functions

--- 07.py
+++ 07.py
@@ -6,4 +6,14 @@ SPLITTER = '^'
 
 start_pos = (0, matrix[0].index(START))
 
-print(start_pos)
+def south(pos):
+    row, col = pos
+    return (row + 1, col) if row < len(matrix) - 1 else None
+
+def southwest(pos):
+    row, col = pos
+    return (row + 1, col - 1) if (row < len(matrix) - 1 and col > 0) else None
+
+def southeast(pos):
+    row, col = pos
+    return (row + 1, col + 1) if (row < len(matrix) - 1 and col < len(matrix[0]) - 1) else None
