580aa4bfa039bb8000c621cdacf4ee32e95bb7ee
Pier-Luc Brault <pier-luc@brault.me>
2025-12-08 21:53:16 -0500

    Refactor

--- 08.py
+++ 08.py
@@ -4,6 +4,17 @@ PART1_NUM_CIRCUITS_TO_KEEP = 3
 import numpy as np
 from functools import reduce
 
+def create_connection_graph(connections):
+    graph = {}
+    for junction_box_1, junction_box_2 in connections:
+        if junction_box_1 not in graph:
+            graph[junction_box_1] = []
+        if junction_box_2 not in graph:
+            graph[junction_box_2] = []
+        graph[junction_box_1].append(junction_box_2)
+        graph[junction_box_2].append(junction_box_1)
+    return graph
+
 junction_boxes = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
 print('Calculating distances...')
@@ -17,38 +28,36 @@ for i, junction_box in enumerate(junction_boxes):
 print('Sorting distances...')
 sorted_distances = {key: value for key, value in sorted(distances.items(), key=lambda item: item[1])}
 
-connections_to_use = list(sorted_distances.keys())[:PART1_NUM_CONNECTIONS_TO_USE]
+def solve_part1():
+    print('=== PART 1 ===')
+    print('Creating graph of connected junction boxes...')
+    connections_to_use = list(sorted_distances.keys())[:PART1_NUM_CONNECTIONS_TO_USE]
+    graph = create_connection_graph(connections_to_use)
 
-print('Creating graph of junction boxes...')
-graph = {}
-for junction_box_1, junction_box_2 in connections_to_use:
-    if junction_box_1 not in graph:
-        graph[junction_box_1] = []
-    if junction_box_2 not in graph:
-        graph[junction_box_2] = []
-    graph[junction_box_1].append(junction_box_2)
-    graph[junction_box_2].append(junction_box_1)
+    print('Computing circuits...')
 
+    circuits = []
+    processed_boxes = set()
 
-print('Computing circuits...')
+    def build_circuit(junction_box, circuit):
+        processed_boxes.add(junction_box)
+        circuit.add(junction_box)
 
-circuits = []
-processed_boxes = set()
+        for other_junction_box in graph[junction_box]:
+            if other_junction_box not in processed_boxes:
+                build_circuit(other_junction_box, circuit)
 
-def build_circuit(junction_box, circuit):
-    processed_boxes.add(junction_box)
-    circuit.add(junction_box)
+    for junction_box in graph:
+        if junction_box not in processed_boxes:
+            circuit = set()
+            build_circuit(junction_box, circuit)
+            circuits.append(circuit)
 
-    for other_junction_box in graph[junction_box]:
-        if other_junction_box not in processed_boxes:
-            build_circuit(other_junction_box, circuit)
+    sorted_circuit_lengths = sorted([len(circuit) for circuit in circuits], reverse=True)
 
-for junction_box in graph:
-    if junction_box not in processed_boxes:
-        circuit = set()
-        build_circuit(junction_box, circuit)
-        circuits.append(circuit)
+    print('Result (part 1):', reduce(
+        lambda acc, circuit_length : acc * circuit_length,
+        sorted_circuit_lengths[:PART1_NUM_CIRCUITS_TO_KEEP])
+    )
 
-sorted_circuit_lengths = sorted([len(circuit) for circuit in circuits], reverse=True)
-
-print('Result (part 1):', reduce(lambda acc, circuit_length : acc * circuit_length, sorted_circuit_lengths[:PART1_NUM_CIRCUITS_TO_KEEP]))
+solve_part1()
