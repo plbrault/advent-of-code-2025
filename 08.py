be94b483c4a4a829445fd2360e94f0b3581418a8
Pier-Luc Brault <pier-luc@brault.me>
2025-12-08 19:46:26 -0500

    Fix graph

--- 08.py
+++ 08.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-NUM_CIRCUITS = 10
+NUM_BOXES_TO_USE = 10
 
 junction_boxes = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
@@ -15,12 +15,16 @@ for i, junction_box in enumerate(junction_boxes):
 print('Sorting distances...')
 sorted_distances = {key: value for key, value in sorted(distances.items(), key=lambda item: item[1])}
 
+boxes_to_use = list(sorted_distances.keys())[:NUM_BOXES_TO_USE]
+
 print('Creating graph of junction boxes...')
 graph = {}
-for junction_box_1, junction_box_2 in list(distances.keys())[:NUM_CIRCUITS]:
+for junction_box_1, junction_box_2 in boxes_to_use:
     if junction_box_1 not in graph:
         graph[junction_box_1] = []
     if junction_box_2 not in graph:
         graph[junction_box_2] = []
     graph[junction_box_1].append(junction_box_2)
     graph[junction_box_2].append(junction_box_1)
+
+print(graph)
