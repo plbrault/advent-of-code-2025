4fdbeb41102184121b1a93a5a2104b926f2fd48c
Pier-Luc Brault <pier-luc@brault.me>
2025-12-08 19:14:13 -0500

    Adding prints for steps

--- 08.py
+++ 08.py
@@ -2,10 +2,13 @@ import numpy as np
 
 junction_boxes = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
+print('Calculating distances...')
+
 distances = {}
 for i, junction_box in enumerate(junction_boxes):
     for _, other_junction_box in enumerate(junction_boxes[i + 1:]):
         distances[(junction_box, other_junction_box)] = np.linalg.norm(
             np.array(junction_box) - np.array(other_junction_box))
 
+print('Sorting distances...')
 sorted_distances = {key: value for key, value in sorted(distances.items(), key=lambda item: item[1])}
