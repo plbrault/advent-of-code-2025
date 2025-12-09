68334ba46e2d7bb28dadbd957f601077b51b323a
Pier-Luc Brault <pier-luc@brault.me>
2025-12-08 19:34:25 -0500

    Create graph

--- 08.py
+++ 08.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+NUM_CIRCUITS = 10
+
 junction_boxes = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
 print('Calculating distances...')
@@ -12,3 +14,13 @@ for i, junction_box in enumerate(junction_boxes):
 
 print('Sorting distances...')
 sorted_distances = {key: value for key, value in sorted(distances.items(), key=lambda item: item[1])}
+
+print('Creating graph of junction boxes...')
+graph = {}
+for junction_box_1, junction_box_2 in list(distances.keys())[:NUM_CIRCUITS]:
+    if junction_box_1 not in graph:
+        graph[junction_box_1] = []
+    if junction_box_2 not in graph:
+        graph[junction_box_2] = []
+    graph[junction_box_1].append(junction_box_2)
+    graph[junction_box_2].append(junction_box_1)
