6571111e3eb26390567251931140a3174dbd0500
Pier-Luc Brault <pier-luc@brault.me>
2025-12-08 19:10:34 -0500

    Calculate and sort distances

--- 08.py
+++ 08.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 junction_boxes = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
-print(junction_boxes)
+distances = {}
+for i, junction_box in enumerate(junction_boxes):
+    for _, other_junction_box in enumerate(junction_boxes[i + 1:]):
+        distances[(junction_box, other_junction_box)] = np.linalg.norm(
+            np.array(junction_box) - np.array(other_junction_box))
+
+sorted_distances = {key: value for key, value in sorted(distances.items(), key=lambda item: item[1])}
