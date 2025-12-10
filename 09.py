d821cc55b5f8f174461470f85565e8ba1158ea8d
Pier-Luc Brault <pier-luc@brault.me>
2025-12-09 21:00:42 -0500

    Create tiles per row and tiles per col maps

--- 09.py
+++ 09.py
@@ -8,3 +8,19 @@ for tile1 in tiles:
         largest_area = max(largest_area, width * height)
 
 print('Result (part 1):', largest_area)
+
+tiles_per_row = {}
+tiles_per_col = {}
+
+for tile in tiles:
+    if tile[1] not in tiles_per_row:
+        tiles_per_row[tile[1]] = []
+    if tile[0] not in tiles_per_col:
+        tiles_per_col[tile[0]] = []
+    tiles_per_row[tile[1]].append(tile)
+    tiles_per_col[tile[0]].append(tile)
+
+for row in tiles_per_row:
+    tiles_per_row[row] = sorted(tiles_per_row[row], key=lambda tile: tile[0])
+for col in tiles_per_col:
+    tiles_per_col[col] = sorted(tiles_per_col[col], key=lambda tile: tile[1])
