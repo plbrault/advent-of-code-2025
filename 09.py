f9400d76c098305f78cfff07cdc2d3487b693dc7
Pier-Luc Brault <pier-luc@brault.me>
2025-12-10 00:24:30 -0500

    Fix other bugs

--- 09.py
+++ 09.py
@@ -35,6 +35,9 @@ for y in x_values_per_y:
 red_tiles = set(tiles)
 
 def is_red_or_green(tile):
+    if tile in red_tiles:
+        return True
+
     is_before_first_x = tile[0] < x_values_per_y[tile[1]][0]
     is_after_last_x = tile[0] > x_values_per_y[tile[1]][-1]
     is_before_first_y = tile[1] < y_values_per_x[tile[0]][0]
@@ -46,29 +49,25 @@ def is_red_or_green(tile):
     ):
         return True
 
-    cond_x = 0
-    cond_y = 0
+    a = b = c = d = False
     if is_before_first_x or is_after_last_x:
-        for x in x_values[:x_values.index(tile[0])-1]:
-            if tile[1] > y_values_per_x[x][0] and tile[1] < y_values_per_x[x][-1]:
-                cond_x += 1
-                break
+        if x_values.index(tile[0]) > 0:
+            for x in x_values[:x_values.index(tile[0])-1]:
+                if tile[1] >= y_values_per_x[x][0] and tile[1] <= y_values_per_x[x][-1]:
+                    a = True
         for x in x_values[x_values.index(tile[0])+1:]:
-            if tile[1] > y_values_per_x[x][0] and tile[1] < y_values_per_x[x][-1]:
-                cond_x += 1
-                break
+            if tile[1] >= y_values_per_x[x][0] and tile[1] <= y_values_per_x[x][-1]:
+                b = True
     if is_before_first_y or is_after_last_y:
-        for y in y_values[:y_values.index(tile[1])-1]:
-            if tile[0] > x_values_per_y[y][0] and tile[0] < x_values_per_y[y][-1]:
-                cond_y += 1
-                break
+        if y_values.index(tile[1]) > 0:
+            for y in y_values[:y_values.index(tile[1])-1]:
+                if tile[0] >= x_values_per_y[y][0] and tile[0] <= x_values_per_y[y][-1]:
+                    c = True
         for y in y_values[y_values.index(tile[1])+1:]:
-            if tile[0] > x_values_per_y[y][0] and tile[0] < x_values_per_y[y][-1]:
-                cond_y += 1
-                break
+            if tile[0] >= x_values_per_y[y][0] and tile[0] <= x_values_per_y[y][-1]:
+                d = True
 
-    print(cond_x, cond_y)
-    return cond_x + cond_y == 4
+    return a and b and c and d
 
 largest_area = 0
 for i, tile1 in enumerate(tiles):
