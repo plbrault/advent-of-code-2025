07119142e1dc7fa27521bd756ef9e5aea65d0115
Pier-Luc Brault <pier-luc@brault.me>
2025-12-09 20:54:56 -0500

    Rename coords to tiles

--- 09.py
+++ 09.py
@@ -1,10 +1,10 @@
-coords = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
+tiles = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
 largest_area = 0
-for coord1 in coords:
-    for coord2 in coords:
-        width = abs(coord2[0] - coord1[0]) + 1
-        height = abs(coord2[1] - coord1[1]) + 1
+for tile1 in tiles:
+    for tile2 in tiles:
+        width = abs(tile2[0] - tile1[0]) + 1
+        height = abs(tile2[1] - tile1[1]) + 1
         largest_area = max(largest_area, width * height)
 
 print('Result (part 1):', largest_area)
