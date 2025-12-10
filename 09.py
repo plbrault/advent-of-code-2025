4ada2212f77c6e2d88d83176d0804a0cee372a4c
Pier-Luc Brault <pier-luc@brault.me>
2025-12-09 21:40:29 -0500

    Restart part 2 attempt

--- 09.py
+++ 09.py
@@ -1,29 +1,35 @@
 tiles = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
 largest_area = 0
-for tile1 in tiles:
-    for tile2 in tiles:
+for i, tile1 in enumerate(tiles):
+    for tile2 in tiles[i + 1:]:
         width = abs(tile2[0] - tile1[0]) + 1
         height = abs(tile2[1] - tile1[1]) + 1
         largest_area = max(largest_area, width * height)
 
 print('Result (part 1):', largest_area)
 
-tiles_per_row = {}
-tiles_per_col = {}
+tiles_per_y = {}
+tiles_per_x = {}
 
 for tile in tiles:
-    if tile[1] not in tiles_per_row:
-        tiles_per_row[tile[1]] = []
-    if tile[0] not in tiles_per_col:
-        tiles_per_col[tile[0]] = []
-    tiles_per_row[tile[1]].append(tile)
-    tiles_per_col[tile[0]].append(tile)
+    if tile[1] not in tiles_per_y:
+        tiles_per_y[tile[1]] = []
+    if tile[0] not in tiles_per_x:
+        tiles_per_x[tile[0]] = []
+    tiles_per_y[tile[1]].append(tile)
+    tiles_per_x[tile[0]].append(tile)
 
-for row in tiles_per_row:
-    tiles_per_row[row] = sorted(tiles_per_row[row], key=lambda tile: tile[0])
-for col in tiles_per_col:
-    tiles_per_col[col] = sorted(tiles_per_col[col], key=lambda tile: tile[1])
+for x in tiles_per_x:
+    tiles_per_x[x] = sorted(tiles_per_x[x], key=lambda tile: tile[1])
+for y in tiles_per_y:
+    tiles_per_y[y] = sorted(tiles_per_y[y], key=lambda tile: tile[0])
 
-rows = sorted(list(tiles_per_row.keys()))
-cols = sorted(list(tiles_per_col.keys()))
+rows = sorted(list(tiles_per_y.keys()))
+cols = sorted(list(tiles_per_x.keys()))
+
+for i, tile1 in enumerate(tiles):
+    for tile2 in tiles[i + 1:]:
+        tile1_x, tile1_y = tile1
+        tile2_x, tile2_y = tile2
+        #print(tile1_x, tile1_y, tile2_x, tile2_y)
