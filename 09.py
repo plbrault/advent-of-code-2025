77ba38532ac087e6a460bc6a75f2aa665a19f866
Pier-Luc Brault <pier-luc@brault.me>
2025-12-09 20:40:43 -0500

    Solve part 1

--- 09.py
+++ 09.py
@@ -1,3 +1,10 @@
 coords = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
 
-print(coords)
+largest_area = 0
+for coord1 in coords:
+    for coord2 in coords:
+        width = abs(coord2[0] - coord1[0]) + 1
+        height = abs(coord2[1] - coord1[1]) + 1
+        largest_area = max(largest_area, width * height)
+
+print('Result (part 1):', largest_area)
