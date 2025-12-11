b83fb0227e9091b4f6ee3f3347624b922a7d8d01
Pier-Luc Brault <pier-luc@brault.me>
2025-12-10 19:14:15 -0500

    Shapely solution works with example

--- 09.py
+++ 09.py
@@ -1,13 +1,26 @@
+from shapely.geometry import Polygon
 tiles = [tuple([int(value) for value in line.split(',')]) for line in open('input.txt').readlines()]
-
 
 rectangles = []
 largest_area = 0
 for i, tile1 in enumerate(tiles):
     for tile2 in tiles[i + 1:]:
-        rectangles.append([tile1, tile2])
+        x1, y1 = tile1
+        x2, y2 = tile2
+        rectangles.append(Polygon([ (x1, y1), (x2, y1), (x2, y2), (x1, y2) ]))
         width = abs(tile2[0] - tile1[0]) + 1
         height = abs(tile2[1] - tile1[1]) + 1
         largest_area = max(largest_area, width * height)
 
 print('Result (part 1):', largest_area)
+
+polygon = Polygon(tiles)
+
+candidate_rectangles = [rectangle for rectangle in rectangles if polygon.contains(rectangle)]
+largest_rectangle = sorted(candidate_rectangles, key=lambda rectangle: rectangle.area, reverse=True)[0]
+x1, y1, x2, y2 = largest_rectangle.bounds
+width = x2 - x1 + 1
+height = y2 - y1 + 1
+largest_area = int(width * height)
+
+print('Result (part 2):', largest_area)
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 My solutions in Python to the [Advent of Code 2025](https://adventofcode.com/2025).
 
-Dependencies are managed by pipenv. Run `pipenv sync` to install.
+Dependencies are managed by pipenv. Run `pipenv sync` to install, then use `pipenv run python XX.py` to run desired script.
 
 License: MIT
