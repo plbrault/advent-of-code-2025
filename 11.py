44e7dc70111a9026bab148e57f167eb12e5bb4ad
Pier-Luc Brault <pier-luc@brault.me>
2025-12-11 22:52:46 -0500

    Memoize

--- 11.py
+++ 11.py
@@ -4,7 +4,12 @@ for line in open('input.txt').readlines():
     neighbours = neighbours_str.split(' ')
     graph[node] = neighbours
 
+memo = {}
+
 def find_paths(start, end, visited_nodes=set()):
+    if (start, end) in memo:
+        return memo[(start, end)]
+
     if not visited_nodes:
         visited_nodes = {start}
     num_paths = 0
@@ -17,6 +22,8 @@ def find_paths(start, end, visited_nodes=set()):
             num_paths += 1
         else:
             num_paths += find_paths(neighbour, end, visited_nodes)
+
+    memo[(start, end)] = num_paths
     return num_paths
 
 print('Result (part 1):', find_paths('you', 'out'))
