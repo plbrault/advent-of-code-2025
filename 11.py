b1b962b9d4c31ae7a2bc1b2b2931e46fe65bb10e
Pier-Luc Brault <pier-luc@brault.me>
2025-12-11 22:49:21 -0500

    Add visited_nodes

--- 11.py
+++ 11.py
@@ -4,13 +4,17 @@ for line in open('input.txt').readlines():
     neighbours = neighbours_str.split(' ')
     graph[node] = neighbours
 
-def find_paths(start, end):
+def find_paths(start, end, visited_nodes=set()):
+    if not visited_nodes:
+        visited_nodes = {start}
     num_paths = 0
     for neighbour in graph[start]:
+        if neighbour in visited_nodes:
+            continue
         if neighbour == end:
             num_paths += 1
         else:
-            num_paths += find_paths(neighbour, end)
+            num_paths += find_paths(neighbour, end, visited_nodes)
     return num_paths
 
 print('Result (part 1):', find_paths('you', 'out'))
