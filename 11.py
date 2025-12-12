f29d29b5461d785773fe9b063129a1e146e0a1bc
Pier-Luc Brault <pier-luc@brault.me>
2025-12-11 20:36:22 -0500

    Solve part 1

--- 11.py
+++ 11.py
@@ -1,9 +1,19 @@
-def parse_input(filename):
-    graph = {}
-    for line in open(filename).readlines():
-        node, neighbours_str = line.replace('\n','').split(': ')
-        neighbours = neighbours_str.split(' ')
-        graph[node] = neighbours
-    return graph
+graph = {}
+for line in open('input.txt').readlines():
+    node, neighbours_str = line.replace('\n','').split(': ')
+    neighbours = neighbours_str.split(' ')
+    graph[node] = neighbours
 
-print(parse_input('input.txt'))
+def solve_part1():
+    def find_paths_to_out(node):
+        num_paths_to_out = 0
+        for neighbour in graph[node]:
+            if neighbour == 'out':
+                return 1
+            else:
+                num_paths_to_out += find_paths_to_out(neighbour)
+        return num_paths_to_out
+    
+    print('Result (part 1):', find_paths_to_out('you'))
+
+solve_part1()
