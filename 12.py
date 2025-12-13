c0cfc204bd11032399d3eaa3a956a0610007bb53
Pier-Luc Brault <pier-luc@brault.me>
2025-12-12 20:59:00 -0500

    Solve part 1

--- 12.py
+++ 12.py
@@ -22,6 +22,30 @@ regions = [
         ]
 ]
 
+GIFT_SIZE = 9
 
+solvable = []
+unsolvable = []
+unknown = []
 
-print(gifts)
+for region in regions:
+    total_gifts = sum(region[1])
+    total_gift_size = total_gifts * GIFT_SIZE
+    total_shape_parts = sum([
+        num_gifts * sum([
+            sum([ 1 for value in shape_row if value == '#' ])
+            for shape_row in gifts[gift_id]
+        ])
+        for gift_id, num_gifts in enumerate(region[1])
+    ])
+    region_size = region[0][0] * region[0][1]
+    if region_size >= total_gift_size:
+        solvable.append(region)
+    elif region_size < total_shape_parts:
+        unsolvable.append(region)
+    else:
+        unknown.append(region)
+
+print('Solvable regions:', len(solvable))
+print('Unsolvable regions:', len(unsolvable))
+print('Unknown:', len(unknown))
